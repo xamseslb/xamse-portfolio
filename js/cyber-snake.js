fa6f075d655333587b99cbba60f6859d8c17380d
xamseslb <xamseslb@gmail.com>
2025-11-05 13:46:19 +0100

    La til DIRECTION-objekt for styring

--- js/cyber-snake.js
+++ js/cyber-snake.js
@@ -30,3 +30,10 @@ let gameRunning = true;
 let lastRenderTime = 0;
 let inputQueue = [];
 
+// Definerer retninger
+const DIRECTION = {
+    UP: { x: 0, y: -1 },
+    DOWN: { x: 0, y: 1 },
+    LEFT: { x: -1, y: 0 },
+    RIGHT: { x: 1, y: 0 }
+};
